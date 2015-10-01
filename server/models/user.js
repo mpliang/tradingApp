f4838a22003cba6685ad8f99fb828cd92356aa31
Darien Lombardi <darien.lombardi@gmail.com>
2015-09-30 20:07:27 -0700

    fixed another problem in user schema

--- server/models/user.js
+++ server/models/user.js
@@ -7,7 +7,7 @@ var userSchema = new Schema({
   username: {type: String, required: true},
   isManager: {type: Boolean,  default: false},
   isAdmin: {type: Boolean,  default: false},
-  apartmentNum: {type: Mongoose.Schema.ObjectId , ref: 'Apartment'};
+  apartmentNum: {type: Mongoose.Schema.ObjectId , ref: 'Apartment'}
 });
 
 userSchema.plugin(passportLocalMongoose);
